1be82a73eac535b7545a69d044f7853ad1df6aaf
Evandro Costa <evandro.souzac@gmail.com>
2026-03-10 14:29:06 -0300

    refactor(application): remove GetUserQuery

--- src/Voltiq.Application/Features/Users/Queries/GetUser/GetUserQuery.cs
+++
@@ -1,6 +0,0 @@
-using MediatR;
-using Voltiq.Domain.Common;
-
-namespace Voltiq.Application.Features.Users.Queries.GetUser;
-
-public sealed record GetUserQuery(Guid Id) : IRequest<Result<GetUserResponse>>;
