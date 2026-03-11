4e1991f7b583cd10e8199359514f28bc46576b15
Evandro Costa <evandro.souzac@gmail.com>
2026-03-11 17:56:04 -0300

    feat(application): update handlers to use GenerateAccessToken and issue refresh tokens on login

--- src/Voltiq.Application/Features/Auth/Commands/Login/LoginCommandHandler.cs
+++ src/Voltiq.Application/Features/Auth/Commands/Login/LoginCommandHandler.cs
@@ -1,7 +1,9 @@
 using MediatR;
 using Voltiq.Application.Common.Interfaces;
 using Voltiq.Domain.Common;
+using Voltiq.Domain.Entities;
 using Voltiq.Domain.Interfaces;
+using Voltiq.Domain.Interfaces.Repositories;
 using Voltiq.Domain.Interfaces.Repositories.User;
 using Voltiq.Domain.ValueObjects;
 using Voltiq.Exceptions.Errors;
@@ -12,7 +14,9 @@ namespace Voltiq.Application.Features.Auth.Commands.Login;
 public sealed class LoginCommandHandler(
     IUserRepository userRepository,
     IPasswordHasher passwordHasher,
-    ITokenService tokenService)
+    ITokenService tokenService,
+    IRefreshTokenRepository refreshTokenRepository,
+    IUnitOfWork unitOfWork)
     : IRequestHandler<LoginCommand, Result<LoginResponse>>
 {
     public async Task<Result<LoginResponse>> Handle(LoginCommand request, CancellationToken cancellationToken)
@@ -25,8 +29,13 @@ public sealed class LoginCommandHandler(
             return Result<LoginResponse>.Failure(
                 new UnauthorizedError(ResourceErrorMessages.LOGIN_CREDENCIAIS_INVALIDAS));
 
-        var token = tokenService.GenerateToken(user.Id.ToString(), user.Name, []);
+        var accessToken = tokenService.GenerateAccessToken(user.Id.ToString(), user.Name, []);
+        var rawRefreshToken = tokenService.GenerateRefreshToken();
 
-        return Result<LoginResponse>.Success(new LoginResponse(token));
+        var refreshToken = RefreshToken.Create(rawRefreshToken, user.Id, expiresInDays: 7);
+        await refreshTokenRepository.AddAsync(refreshToken, cancellationToken);
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return Result<LoginResponse>.Success(new LoginResponse(accessToken, rawRefreshToken));
     }
 }
