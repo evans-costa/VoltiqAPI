f4e4612713decbdc0f3855041b9e098b14b4309a
Evandro Costa <evandro.souzac@gmail.com>
2026-03-11 17:55:41 -0300

    feat(domain): add IRefreshTokenRepository interface

---
+++ src/Voltiq.Domain/Interfaces/Repositories/IRefreshTokenRepository.cs
@@ -0,0 +1,9 @@
+using Voltiq.Domain.Entities;
+
+namespace Voltiq.Domain.Interfaces.Repositories;
+
+public interface IRefreshTokenRepository
+{
+    Task<RefreshToken?> GetByTokenAsync(string token, CancellationToken cancellationToken = default);
+    Task AddAsync(RefreshToken refreshToken, CancellationToken cancellationToken = default);
+}
