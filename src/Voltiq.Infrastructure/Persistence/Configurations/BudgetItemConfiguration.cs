93acde7eb9df26604a56c9dea60ff83029e9d375
Evandro Costa <evandro.souzac@gmail.com>
2026-03-14 22:51:51 -0300

    feat(infrastructure): add BudgetItemConfiguration

---
+++ src/Voltiq.Infrastructure/Persistence/Configurations/BudgetItemConfiguration.cs
@@ -0,0 +1,47 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Voltiq.Domain.Entities;
+
+namespace Voltiq.Infrastructure.Persistence.Configurations;
+
+public sealed class BudgetItemConfiguration : IEntityTypeConfiguration<BudgetItem>
+{
+    public void Configure(EntityTypeBuilder<BudgetItem> builder)
+    {
+        builder.HasKey(i => i.Id);
+
+        builder.Property(i => i.BudgetId).IsRequired();
+
+        builder.Property(i => i.MaterialId);
+
+        builder.Property(i => i.MaterialName)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(i => i.Unit)
+            .HasMaxLength(20);
+
+        builder.Property(i => i.Quantity).IsRequired();
+
+        builder.Property(i => i.UnitPrice)
+            .IsRequired()
+            .HasPrecision(18, 2);
+
+        builder.Property(i => i.TotalPrice)
+            .IsRequired()
+            .HasPrecision(18, 2);
+
+        builder.HasOne<Material>()
+            .WithMany()
+            .HasForeignKey(i => i.MaterialId)
+            .OnDelete(DeleteBehavior.SetNull);
+
+        builder.HasIndex(i => i.BudgetId)
+            .HasDatabaseName("IX_BudgetItems_BudgetId");
+
+        builder.HasIndex(i => i.MaterialId)
+            .HasDatabaseName("IX_BudgetItems_MaterialId");
+
+        builder.Ignore(i => i.DomainEvents);
+    }
+}
