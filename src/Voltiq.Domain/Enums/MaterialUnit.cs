ab7cd8b467258e48362f732f9c489d1b104796d4
Evandro Costa <evandro.souzac@gmail.com>
2026-03-14 23:27:05 -0300

    feat(domain): add MaterialUnit enum

---
+++ src/Voltiq.Domain/Enums/MaterialUnit.cs
@@ -0,0 +1,12 @@
+namespace Voltiq.Domain.Enums;
+
+public enum MaterialUnit
+{
+    Unidade = 1,
+    Metro = 2,
+    MetroQuadrado = 3,
+    Rolo = 4,
+    Par = 5,
+    Caixa = 6,
+    Quilograma = 7,
+}
