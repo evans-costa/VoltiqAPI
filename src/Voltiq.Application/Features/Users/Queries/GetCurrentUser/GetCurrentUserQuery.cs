13b80557b33cec5c74ddf7d1a73eafb2235093df
Evandro Costa <evandro.souzac@gmail.com>
2026-03-30 21:54:30 -0300

    refactor(users): migrate GetCurrentUserQuery to IAuthenticatedRequest

--- src/Voltiq.Application/Features/Users/Queries/GetCurrentUser/GetCurrentUserQuery.cs
+++ src/Voltiq.Application/Features/Users/Queries/GetCurrentUser/GetCurrentUserQuery.cs
@@ -1,6 +1,10 @@
 using ErrorOr;
-using MediatR;
+using Voltiq.Application.Common.Interfaces;
+using Voltiq.Application.Features.Users.Queries.GetCurrentUser;
 
 namespace Voltiq.Application.Features.Users.Queries.GetCurrentUser;
 
-public sealed record GetCurrentUserQuery : IRequest<ErrorOr<GetUserResponse>>;
+public sealed record GetCurrentUserQuery : IAuthenticatedRequest<ErrorOr<GetUserResponse>>
+{
+    public Guid UserId { get; set; }
+}
--- src/Voltiq.Application/Features/Users/Queries/GetCurrentUser/GetCurrentUserQueryHandler.cs
+++ src/Voltiq.Application/Features/Users/Queries/GetCurrentUser/GetCurrentUserQueryHandler.cs
@@ -1,6 +1,5 @@
 using ErrorOr;
 using MediatR;
-using Voltiq.Application.Common.Interfaces;
 using Voltiq.Application.Mappings.Users;
 using Voltiq.Domain.Entities;
 using Voltiq.Domain.Interfaces.Repositories;
@@ -8,24 +7,16 @@ using Voltiq.Exceptions.Resources;
 
 namespace Voltiq.Application.Features.Users.Queries.GetCurrentUser;
 
-public sealed class GetCurrentUserQueryHandler(
-    ICurrentUserService currentUserService,
-    IRepository<User> userRepository)
+public sealed class GetCurrentUserQueryHandler(IRepository<User> userRepository)
     : IRequestHandler<GetCurrentUserQuery, ErrorOr<GetUserResponse>>
 {
     public async Task<ErrorOr<GetUserResponse>> Handle(GetCurrentUserQuery request, CancellationToken cancellationToken)
     {
-        var userId = currentUserService.UserId;
-
-        if (userId == Guid.Empty)
-            return Error.NotFound(description: string.Format(
-                ResourceErrorMessages.ENTIDADE_NAO_ENCONTRADA, nameof(User), currentUserService.UserId));
-
-        var user = await userRepository.GetByIdAsync(userId, cancellationToken);
+        var user = await userRepository.GetByIdAsync(request.UserId, cancellationToken);
 
         if (user is null)
             return Error.NotFound(description: string.Format(
-                ResourceErrorMessages.ENTIDADE_NAO_ENCONTRADA, nameof(User), userId));
+                ResourceErrorMessages.ENTIDADE_NAO_ENCONTRADA, nameof(User), request.UserId));
 
         return user.ToGetUserResponse();
     }
